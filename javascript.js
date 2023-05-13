86dfe762ba69f5306a07bb6da0cd117b0c379425
EDWINA <edwina.addo@icloud.com>
2023-05-13 16:26:29 +0000

    first commit

---
+++ javascript.js
@@ -0,0 +1,43 @@
+
+var name ="edwina";
+var school ="hello world";
+var hospital ="our world";
+var student ="your world";
+var world ="his world";
+
+var age =65;
+var grade =100;
+var number =057357264;
+var bankaccount =6788547;
+var id ="10072561";
+
+var scores =100.5;
+var length =6.7;
+var weight= 100.6;
+var money = 10000000.64;
+var length= 789.9;
+
+
+var correct= true;
+var incorrect=false;
+var right =true;
+var wrong= false;
+var positive=true;
+
+
+document.write('scores')
+document.write ("length")
+document.write ("weight")
+document.write("bankaccount")
+document.write ("money")
+document.write(wrong)
+document.write(name)
+document.write(school)
+document.write(hospital)
+document.write(grade)
+
+
+
+
+
+

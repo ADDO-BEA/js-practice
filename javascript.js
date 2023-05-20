db09bf323203c567efb9bc66d617d50a71adb8a2
EDWINA <edwina.addo@icloud.com>
2023-05-20 14:50:32 +0000

    add javascript.js

--- javascript.js
+++ javascript.js
@@ -1,21 +1,39 @@
 
 var name ="edwina"
-var name ="hello world"
-var name ="our world"
-var name ="your world"
-var name ="his world"
+var school="hello world"
+var hospital ="our world"
+var student ="your world"
+var world ="his world"
 
 var age ="65"
-var age ="73"
-var age ="18"
-var age ="67"
-var age ="86"
+var grade="73"
+var number ="18"
+var backaccount ="67"
+var id ="8666374"
 
-var age ="666.76888"
-var age ="6676.76888"
-var age ="6896.76888"
-var age ="6697.7680648"		
-var age ="6788487.675"
+var scores ="666.76888"
+var length ="6676.76888"
+var weight="6896.76888"
+var money ="6697.7680648"		
+var height ="6788487.675"
+  
+
+var correct = true;
+var incorrect = false;
+var wrong =false;
+var right = true;
+var positive =true;
+
+document .write( age);
+document .write( grade);
+document .write( number);
+document .write( name);
+document .write( school);
+document .write( hospital);
+document .write( scores);
+document .write( length);
+document .write( correct);
+document .write( incorrect);
 
 
 

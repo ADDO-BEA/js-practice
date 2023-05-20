c9a480222747b59b48f8a2144bc7a0a9b0f375ae
EDWINA <edwina.addo@icloud.com>
2023-05-20 14:31:00 +0000

    add myscript2.js

---
+++ myscript2.js
@@ -0,0 +1,39 @@
+
+
+console.log(5 + 9);
+console.log(56 + 67);
+console.log(65 + 67);
+
+console.log(67 - 79);
+console.log( 56 - 8254);
+console.log(562- 75547);
+
+console.log(458 * 45);
+console.log(6 * 84);
+console.log(476 * 1);
+
+
+
+console.log(45 == 45);
+console.log(56 ==56);
+console.log(56 == 65)
+
+console.log( "766" === "765");
+console.log("mango" === "Mangos")
+console.log(78 === 78)
+
+console.log(67 != 78 );
+console.log(67 !=67)
+console.log (00 != 00 )
+
+console.log(78 != 67 && 67 != 95)
+console.log(54 != 76 && 63 != 73)
+console.log (64 != 64 && 46 !=46)
+
+console.log( 3456 === 6537 || 642 === 642 )
+console.log(567==567 || 543 === 543)
+console.log(67 === 765 || 654 === 876)
+
+console.log( 6 * !9 )
+console.log(!67 * 56 )
+console.log( 83 + !5)

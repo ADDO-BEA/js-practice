e37a41d9d78ee90e5cd2fad7c6133604b89769fe
EDWINA <edwina.addo@icloud.com>
2023-05-27 15:21:09 +0000

    add myscript3.js

---
+++ activity.js
@@ -0,0 +1,8 @@
+scores=125
+height=7.78
+length=672
+grade=100
+school="st peters"
+
+
+console.log(scores)
---
+++ myscript3.js
@@ -0,0 +1,40 @@
+let colleagues=["Eddy","Lizzy","Daniel","kofi","Dorcas",]
+console.log(colleagues)
+console.log(colleagues[4])
+
+for (let i = 0; i <colleagues.length; i++) {
+    console.log(colleagues[1]);
+}
+ for (let i = 0; i <colleagues.length; i++){
+    console.log(colleagues[0]);
+ }
+ for(let i =0; i<colleagues.length; i++ ){
+    console.log(colleagues[2]);
+ }
+ for(let i =0; i<colleagues.length; i++){
+    console.log(colleagues[3]);
+ }
+ for( let i=0; i<colleagues.length; i++){
+    console.log(colleagues[4]);
+ }
+
+
+ 
+ let name="eddy"
+ 
+ if (name == "eddy"){
+    console.log("your are right")
+ }else if(name == "edwina"){
+    console.log("your are wrong")
+}else if (name == "Addo"){
+    console.log("your not right")
+}else if (name == "Daniel"){
+    console.log("your not right")
+}
+
+ let  cat = true;
+ if  ( cat == true){
+    console.log("your correct")
+ }else{
+    console.log("your not correct")
+ }
